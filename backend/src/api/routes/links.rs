fe9fa09d32d006a0258cda9e6bee3e6563bd2a59
merlinfuchs <merlinfuchs2001@gmail.com>
2022-08-06 21:43:43 +0200

    add login scopes to invite link

--- backend/src/api/routes/links.rs
+++ backend/src/api/routes/links.rs
@@ -1,5 +1,6 @@
-use actix_web::http::StatusCode;
 use actix_web::{get, HttpResponseBuilder, Responder};
+use actix_web::http::StatusCode;
+use percent_encoding::{NON_ALPHANUMERIC, utf8_percent_encode};
 
 use crate::config::CONFIG;
 
@@ -19,7 +20,11 @@ pub async fn route_link_source() -> impl Responder {
 
 #[get("/link/invite")]
 pub async fn route_link_invite() -> impl Responder {
-    let url = format!("https://discord.com/api/oauth2/authorize?client_id={}&permissions=536871936&scope=bot%20applications.commands", CONFIG.discord.oauth_client_id);
+    let url = format!(
+        "https://discord.com/api/oauth2/authorize?client_id={}&permissions=536871936&scope=identify%20guilds%20bot%20applications.commands&prompt=none&response_type=code&redirect_uri={}",
+        CONFIG.discord.oauth_client_id,
+        utf8_percent_encode(&CONFIG.discord.oauth_redirect_uri, NON_ALPHANUMERIC)
+    );
 
     HttpResponseBuilder::new(StatusCode::FOUND)
         .append_header(("Location", url))
