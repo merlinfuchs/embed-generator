9e12b3c2fd6de89fa1834c8cf3abc655e887c919
merlinfuchs <merlinfuchs2001@gmail.com>
2023-05-09 18:26:42 +0200

    implement action handling

--- embedg-server/actions/handler/handle.go
+++ embedg-server/actions/handler/handle.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"strings"
 
 	"github.com/merlinfuchs/discordgo"
@@ -45,28 +46,37 @@ func (m *ActionHandler) HandleActionInteraction(s *discordgo.Session, i *discord
 	}
 
 	responded := false
+	respond := func(data *discordgo.InteractionResponseData) {
+		var err error
+
+		if !responded {
+			err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: data,
+			})
+		} else {
+			_, err = s.FollowupMessageCreate(i.Interaction, false, &discordgo.WebhookParams{
+				Content:    data.Content,
+				Embeds:     data.Embeds,
+				Components: data.Components,
+				Files:      data.Files,
+			})
+		}
+
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to respond to interaction")
+		} else {
+			responded = true
+		}
+	}
+
 	for _, action := range actionSet.Actions {
 		switch action.Type {
 		case actions.ActionTypeTextResponse:
-			var err error
-			if !responded {
-				err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-					Type: discordgo.InteractionResponseChannelMessageWithSource,
-					Data: &discordgo.InteractionResponseData{
-						Content: action.Text,
-						Flags:   discordgo.MessageFlagsEphemeral,
-					},
-				})
-			} else {
-				_, err = s.FollowupMessageCreate(i.Interaction, false, &discordgo.WebhookParams{
-					Content: action.Text,
-				})
-			}
-			if err != nil {
-				log.Error().Err(err).Msg("Failed to respond to interaction")
-			} else {
-				responded = true
-			}
+			respond(&discordgo.InteractionResponseData{
+				Content: action.Text,
+				Flags:   discordgo.MessageFlagsEphemeral,
+			})
 		case actions.ActionTypeToggleRole:
 			hasRole := false
 			for _, roleID := range i.Member.Roles {
@@ -78,26 +88,52 @@ func (m *ActionHandler) HandleActionInteraction(s *discordgo.Session, i *discord
 			var err error
 			if hasRole {
 				err = s.GuildMemberRoleRemove(i.GuildID, i.Member.User.ID, action.TargetID)
+				if err == nil {
+					respond(&discordgo.InteractionResponseData{
+						Content: fmt.Sprintf("Removed role <@&%s>", action.TargetID),
+						Flags:   discordgo.MessageFlagsEphemeral,
+					})
+				}
 			} else {
 				err = s.GuildMemberRoleAdd(i.GuildID, i.Member.User.ID, action.TargetID)
+				if err == nil {
+					respond(&discordgo.InteractionResponseData{
+						Content: fmt.Sprintf("Added role <@&%s>", action.TargetID),
+						Flags:   discordgo.MessageFlagsEphemeral,
+					})
+				}
 			}
 			if err != nil {
 				log.Error().Err(err).Msg("Failed to toggle role")
+			}
+		case actions.ActionTypeAddRole:
+			err := s.GuildMemberRoleAdd(i.GuildID, i.Member.User.ID, action.TargetID)
+			if err == nil {
+				respond(&discordgo.InteractionResponseData{
+					Content: fmt.Sprintf("Added role <@&%s>", action.TargetID),
+					Flags:   discordgo.MessageFlagsEphemeral,
+				})
+			} else {
+				log.Error().Err(err).Msg("Failed to add role")
+			}
+		case actions.ActionTypeRemoveRole:
+			err := s.GuildMemberRoleRemove(i.GuildID, i.Member.User.ID, action.TargetID)
+			if err == nil {
+				respond(&discordgo.InteractionResponseData{
+					Content: fmt.Sprintf("Removed role <@&%s>", action.TargetID),
+					Flags:   discordgo.MessageFlagsEphemeral,
+				})
+			} else {
+				log.Error().Err(err).Msg("Failed to remove role")
 			}
 		}
 	}
 
 	if !responded {
-		err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "No response",
-				Flags:   discordgo.MessageFlagsEphemeral,
-			},
+		respond(&discordgo.InteractionResponseData{
+			Content: "No response",
+			Flags:   discordgo.MessageFlagsEphemeral,
 		})
-		if err != nil {
-			log.Error().Err(err).Msg("Failed to respond to interaction")
-		}
 	}
 
 	return nil
