b89fad55015bafe9943d975dab00b66622f0f90a
Merlin <merlinfuchs2001@gmail.com>
2024-05-21 21:16:09 +0200

    patch in new db indexes

--- embedg-server/db/postgres/migrations/010_create_db_indexes.up.sql
+++ embedg-server/db/postgres/migrations/010_create_db_indexes.up.sql
@@ -2,3 +2,6 @@ CREATE INDEX ON saved_messages (guild_id);
 CREATE INDEX ON saved_messages (creator_id);
 
 CREATE INDEX ON custom_commands (guild_id);
+
+CREATE INDEX ON message_action_sets (message_id);
+CREATE INDEX ON message_action_sets (set_id);
