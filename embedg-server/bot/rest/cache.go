7c69059265b5d4dea84fc6fe97c89302d2c8fb57
merlinfuchs <merlinfuchs2001@gmail.com>
2025-09-12 15:13:23 -0600

    queue requests for same cache key

--- embedg-server/bot/rest/cache.go
+++ embedg-server/bot/rest/cache.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
+	"sync"
 	"time"
 
 	"github.com/jellydator/ttlcache/v3"
@@ -13,53 +15,76 @@ import (
 	"golang.org/x/oauth2"
 )
 
-type memberCacheKey struct {
-	guildID string
-	userID  string
+func guildCacheKey(guildID string) string {
+	return fmt.Sprintf("guild:%s", guildID)
 }
 
-type oauthGuildsCacheKey string
+func guildChannelsCacheKey(guildID string) string {
+	return fmt.Sprintf("guildChannels:%s", guildID)
+}
 
-type guildCacheKey string
+func guildThreadsCacheKey(guildID string) string {
+	return fmt.Sprintf("guildThreads:%s", guildID)
+}
 
-type channelCacheKey string
+func guildRolesCacheKey(guildID string) string {
+	return fmt.Sprintf("guildRoles:%s", guildID)
+}
 
-type guildChannelsCacheKey string
+func channelCacheKey(channelID string) string {
+	return fmt.Sprintf("channel:%s", channelID)
+}
 
-type guildThreadsCacheKey string
+func oauthUserCacheKey(accessToken string) string {
+	return fmt.Sprintf("oauthUser:%s", accessToken)
+}
 
-type guildRolesCacheKey string
+func oauthUserGuildsCacheKey(accessToken string) string {
+	return fmt.Sprintf("oauthUserGuilds:%s", accessToken)
+}
 
-type oauthUserCacheKey string
+func memberCacheKey(guildID string, userID string) string {
+	return fmt.Sprintf("member:%s:%s", guildID, userID)
+}
 
-type oauthUserGuildsCacheKey string
+// pendingRequest represents an ongoing request for a specific cache key
+type pendingRequest struct {
+	waitGroup sync.WaitGroup
+	result    interface{}
+	err       error
+}
 
 type RestClientWithCache struct {
 	session *discordgo.Session
 
-	oauthGuildsCache     *ttlcache.Cache[oauthGuildsCacheKey, []*discordgo.Guild]
-	guildCache           *ttlcache.Cache[guildCacheKey, *discordgo.Guild]
-	channelCache         *ttlcache.Cache[channelCacheKey, *discordgo.Channel]
-	guildChannelCache    *ttlcache.Cache[guildChannelsCacheKey, []*discordgo.Channel]
-	guildThreadsCache    *ttlcache.Cache[guildThreadsCacheKey, []*discordgo.Channel]
-	guildRolesCache      *ttlcache.Cache[guildRolesCacheKey, []*discordgo.Role]
-	memberCache          *ttlcache.Cache[memberCacheKey, *discordgo.Member]
-	oauthUserCache       *ttlcache.Cache[oauthUserCacheKey, *discordgo.User]
-	oauthUserGuildsCache *ttlcache.Cache[oauthUserGuildsCacheKey, []*OauthUserGuild]
+	oauthGuildsCache     *ttlcache.Cache[string, []*discordgo.Guild]
+	guildCache           *ttlcache.Cache[string, *discordgo.Guild]
+	channelCache         *ttlcache.Cache[string, *discordgo.Channel]
+	guildChannelCache    *ttlcache.Cache[string, []*discordgo.Channel]
+	guildThreadsCache    *ttlcache.Cache[string, []*discordgo.Channel]
+	guildRolesCache      *ttlcache.Cache[string, []*discordgo.Role]
+	memberCache          *ttlcache.Cache[string, *discordgo.Member]
+	oauthUserCache       *ttlcache.Cache[string, *discordgo.User]
+	oauthUserGuildsCache *ttlcache.Cache[string, []*OauthUserGuild]
+
+	// Single mutex to protect all pending request maps
+	pendingMutex sync.RWMutex
+	pendingReqs  map[string]*pendingRequest
 }
 
 func NewRestClientWithCache(session *discordgo.Session) *RestClientWithCache {
 	rest := &RestClientWithCache{
 		session:              session,
-		oauthGuildsCache:     ttlcache.New(ttlcache.WithTTL[oauthGuildsCacheKey, []*discordgo.Guild](5 * time.Minute)),
-		guildCache:           ttlcache.New(ttlcache.WithTTL[guildCacheKey, *discordgo.Guild](15 * time.Minute)),
-		channelCache:         ttlcache.New(ttlcache.WithTTL[channelCacheKey, *discordgo.Channel](3 * time.Minute)),
-		guildChannelCache:    ttlcache.New(ttlcache.WithTTL[guildChannelsCacheKey, []*discordgo.Channel](3 * time.Minute)),
-		guildThreadsCache:    ttlcache.New(ttlcache.WithTTL[guildThreadsCacheKey, []*discordgo.Channel](3 * time.Minute)),
-		guildRolesCache:      ttlcache.New(ttlcache.WithTTL[guildRolesCacheKey, []*discordgo.Role](3 * time.Minute)),
-		memberCache:          ttlcache.New(ttlcache.WithTTL[memberCacheKey, *discordgo.Member](3 * time.Minute)),
-		oauthUserCache:       ttlcache.New(ttlcache.WithTTL[oauthUserCacheKey, *discordgo.User](3 * time.Minute)),
-		oauthUserGuildsCache: ttlcache.New(ttlcache.WithTTL[oauthUserGuildsCacheKey, []*OauthUserGuild](3 * time.Minute)),
+		oauthGuildsCache:     ttlcache.New(ttlcache.WithTTL[string, []*discordgo.Guild](5 * time.Minute)),
+		guildCache:           ttlcache.New(ttlcache.WithTTL[string, *discordgo.Guild](15 * time.Minute)),
+		channelCache:         ttlcache.New(ttlcache.WithTTL[string, *discordgo.Channel](3 * time.Minute)),
+		guildChannelCache:    ttlcache.New(ttlcache.WithTTL[string, []*discordgo.Channel](3 * time.Minute)),
+		guildThreadsCache:    ttlcache.New(ttlcache.WithTTL[string, []*discordgo.Channel](3 * time.Minute)),
+		guildRolesCache:      ttlcache.New(ttlcache.WithTTL[string, []*discordgo.Role](3 * time.Minute)),
+		memberCache:          ttlcache.New(ttlcache.WithTTL[string, *discordgo.Member](3 * time.Minute)),
+		oauthUserCache:       ttlcache.New(ttlcache.WithTTL[string, *discordgo.User](3 * time.Minute)),
+		oauthUserGuildsCache: ttlcache.New(ttlcache.WithTTL[string, []*OauthUserGuild](3 * time.Minute)),
+		pendingReqs:          make(map[string]*pendingRequest),
 	}
 
 	go rest.oauthGuildsCache.Start()
@@ -75,128 +100,141 @@ func NewRestClientWithCache(session *discordgo.Session) *RestClientWithCache {
 	return rest
 }
 
+// getOrSet executes a fetch function with concurrency control
+func getOrSet[T any](c *RestClientWithCache, cacheKey string, cache *ttlcache.Cache[string, T], fetchFunc func() (T, error)) (T, error) {
+	var zero T
+
+	// Check cache first
+	cacheItem := cache.Get(cacheKey)
+	if cacheItem != nil {
+		return cacheItem.Value(), nil
+	}
+
+	// Check if there's already a pending request
+	c.pendingMutex.Lock()
+	if pending, exists := c.pendingReqs[cacheKey]; exists {
+		c.pendingMutex.Unlock()
+		pending.waitGroup.Wait()
+		if pending.err != nil {
+			return zero, pending.err
+		}
+		return pending.result.(T), nil
+	}
+
+	// Create new pending request
+	pending := &pendingRequest{}
+	pending.waitGroup.Add(1)
+	c.pendingReqs[cacheKey] = pending
+	c.pendingMutex.Unlock()
+
+	// Perform the actual request
+	result, err := fetchFunc()
+
+	// Clean up pending request and set result
+	c.pendingMutex.Lock()
+	delete(c.pendingReqs, cacheKey)
+	c.pendingMutex.Unlock()
+
+	pending.result = result
+	pending.err = err
+	pending.waitGroup.Done()
+
+	if err != nil {
+		return zero, err
+	}
+
+	cache.Set(cacheKey, result, 0)
+	return result, nil
+}
+
 func (c *RestClientWithCache) Request(ctx context.Context, method string, url string, body io.Reader, options ...discordgo.RequestOption) ([]byte, error) {
 	options = append(options, discordgo.WithContext(ctx))
 	return c.session.Request(method, url, body, options...)
 }
 
 func (c *RestClientWithCache) Guild(ctx context.Context, guildID string) (*discordgo.Guild, error) {
-	cacheKey := guildCacheKey(guildID)
-	cacheItem := c.guildCache.Get(cacheKey)
-	if cacheItem != nil {
-		return cacheItem.Value(), nil
-	}
-
-	guild, err := c.session.Guild(guildID)
-	if err != nil {
-		if util.IsDiscordRestErrorCode(err, discordgo.ErrCodeUnknownGuild, discordgo.ErrCodeMissingAccess) {
-			return nil, ErrNotFound
+	return getOrSet(c, guildCacheKey(guildID), c.guildCache, func() (*discordgo.Guild, error) {
+		guild, err := c.session.Guild(guildID)
+		if err != nil {
+			if util.IsDiscordRestErrorCode(err, discordgo.ErrCodeUnknownGuild, discordgo.ErrCodeMissingAccess) {
+				return nil, ErrNotFound
+			}
+			return nil, err
 		}
-		return nil, err
-	}
-
-	c.guildCache.Set(cacheKey, guild, 0)
-	return guild, nil
+		return guild, nil
+	})
 }
 
 func (c *RestClientWithCache) GuildChannels(ctx context.Context, guildID string) ([]*discordgo.Channel, error) {
-	cacheKey := guildChannelsCacheKey(guildID)
-	cacheItem := c.guildChannelCache.Get(cacheKey)
-	if cacheItem != nil {
-		return cacheItem.Value(), nil
-	}
-
-	channels, err := c.session.GuildChannels(guildID, discordgo.WithContext(ctx))
-	if err != nil {
-		if util.IsDiscordRestErrorCode(err, discordgo.ErrCodeUnknownGuild, discordgo.ErrCodeMissingAccess) {
-			return nil, ErrNotFound
+	return getOrSet(c, guildChannelsCacheKey(guildID), c.guildChannelCache, func() ([]*discordgo.Channel, error) {
+		channels, err := c.session.GuildChannels(guildID, discordgo.WithContext(ctx))
+		if err != nil {
+			if util.IsDiscordRestErrorCode(err, discordgo.ErrCodeUnknownGuild, discordgo.ErrCodeMissingAccess) {
+				return nil, ErrNotFound
+			}
+			return nil, err
 		}
-		return nil, err
-	}
 
-	for _, channel := range channels {
-		c.channelCache.Set(channelCacheKey(channel.ID), channel, 0)
-	}
+		// Cache individual channels
+		for _, channel := range channels {
+			c.channelCache.Set(channel.ID, channel, 0)
+		}
 
-	c.guildChannelCache.Set(cacheKey, channels, 0)
-	return channels, nil
+		return channels, nil
+	})
 }
 
 func (c *RestClientWithCache) Channel(ctx context.Context, channelID string) (*discordgo.Channel, error) {
-	cacheKey := channelCacheKey(channelID)
-	cacheItem := c.channelCache.Get(cacheKey)
-	if cacheItem != nil {
-		return cacheItem.Value(), nil
-	}
-
-	channel, err := c.session.Channel(channelID, discordgo.WithContext(ctx))
-	if err != nil {
-		if util.IsDiscordRestErrorCode(err, discordgo.ErrCodeUnknownChannel, discordgo.ErrCodeMissingAccess) {
-			return nil, ErrNotFound
+	return getOrSet(c, channelCacheKey(channelID), c.channelCache, func() (*discordgo.Channel, error) {
+		channel, err := c.session.Channel(channelID, discordgo.WithContext(ctx))
+		if err != nil {
+			if util.IsDiscordRestErrorCode(err, discordgo.ErrCodeUnknownChannel, discordgo.ErrCodeMissingAccess) {
+				return nil, ErrNotFound
+			}
+			return nil, err
 		}
-		return nil, err
-	}
-
-	c.channelCache.Set(cacheKey, channel, 0)
-	return channel, nil
+		return channel, nil
+	})
 }
 
 func (c *RestClientWithCache) GuildThreads(ctx context.Context, guildID string) ([]*discordgo.Channel, error) {
-	cacheKey := guildThreadsCacheKey(guildID)
-	cacheItem := c.guildThreadsCache.Get(cacheKey)
-	if cacheItem != nil {
-		return cacheItem.Value(), nil
-	}
-
-	// TODO: Paginate
-	res, err := c.session.GuildThreadsActive(guildID, discordgo.WithContext(ctx))
-	if err != nil {
-		if util.IsDiscordRestErrorCode(err, discordgo.ErrCodeUnknownGuild, discordgo.ErrCodeMissingAccess) {
-			return nil, ErrNotFound
+	return getOrSet(c, guildThreadsCacheKey(guildID), c.guildThreadsCache, func() ([]*discordgo.Channel, error) {
+		// TODO: Paginate
+		res, err := c.session.GuildThreadsActive(guildID, discordgo.WithContext(ctx))
+		if err != nil {
+			if util.IsDiscordRestErrorCode(err, discordgo.ErrCodeUnknownGuild, discordgo.ErrCodeMissingAccess) {
+				return nil, ErrNotFound
+			}
+			return nil, err
 		}
-		return nil, err
-	}
-
-	c.guildThreadsCache.Set(cacheKey, res.Threads, 0)
-	return res.Threads, nil
+		return res.Threads, nil
+	})
 }
 
 func (c *RestClientWithCache) GuildMember(ctx context.Context, guildID string, userID string) (*discordgo.Member, error) {
-	cacheKey := memberCacheKey{guildID: guildID, userID: userID}
-	cacheItem := c.memberCache.Get(cacheKey)
-	if cacheItem != nil {
-		return cacheItem.Value(), nil
-	}
-
-	member, err := c.session.GuildMember(guildID, userID, discordgo.WithContext(ctx))
-	if err != nil {
-		if util.IsDiscordRestErrorCode(err, discordgo.ErrCodeUnknownMember, discordgo.ErrCodeUnknownGuild, discordgo.ErrCodeMissingAccess) {
-			return nil, ErrNotFound
+	return getOrSet(c, memberCacheKey(guildID, userID), c.memberCache, func() (*discordgo.Member, error) {
+		member, err := c.session.GuildMember(guildID, userID, discordgo.WithContext(ctx))
+		if err != nil {
+			if util.IsDiscordRestErrorCode(err, discordgo.ErrCodeUnknownMember, discordgo.ErrCodeUnknownGuild, discordgo.ErrCodeMissingAccess) {
+				return nil, ErrNotFound
+			}
+			return nil, err
 		}
-		return nil, err
-	}
-
-	c.memberCache.Set(cacheKey, member, 0)
-	return member, nil
+		return member, nil
+	})
 }
 
 func (c *RestClientWithCache) GuildRoles(ctx context.Context, guildID string) ([]*discordgo.Role, error) {
-	cacheKey := guildRolesCacheKey(guildID)
-	cacheItem := c.guildRolesCache.Get(cacheKey)
-	if cacheItem != nil {
-		return cacheItem.Value(), nil
-	}
-
-	roles, err := c.session.GuildRoles(guildID, discordgo.WithContext(ctx))
-	if err != nil {
-		if util.IsDiscordRestErrorCode(err, discordgo.ErrCodeUnknownGuild, discordgo.ErrCodeMissingAccess) {
-			return nil, ErrNotFound
+	return getOrSet(c, guildRolesCacheKey(guildID), c.guildRolesCache, func() ([]*discordgo.Role, error) {
+		roles, err := c.session.GuildRoles(guildID, discordgo.WithContext(ctx))
+		if err != nil {
+			if util.IsDiscordRestErrorCode(err, discordgo.ErrCodeUnknownGuild, discordgo.ErrCodeMissingAccess) {
+				return nil, ErrNotFound
+			}
+			return nil, err
 		}
-		return nil, err
-	}
-
-	c.guildRolesCache.Set(cacheKey, roles, 0)
-	return roles, nil
+		return roles, nil
+	})
 }
 
 func (c *RestClientWithCache) GuildRole(ctx context.Context, guildID string, roleID string) (*discordgo.Role, error) {
@@ -218,58 +256,48 @@ func (c *RestClientWithCache) GuildRole(ctx context.Context, guildID string, rol
 }
 
 func (c *RestClientWithCache) OauthUser(ctx context.Context, accessToken string) (*discordgo.User, error) {
-	cacheKey := oauthUserCacheKey(accessToken)
-	cacheItem := c.oauthUserCache.Get(cacheKey)
-	if cacheItem != nil {
-		return cacheItem.Value(), nil
-	}
+	return getOrSet(c, oauthUserCacheKey(accessToken), c.oauthUserCache, func() (*discordgo.User, error) {
+		client := (&oauth2.Config{}).Client(ctx, &oauth2.Token{AccessToken: accessToken})
 
-	client := (&oauth2.Config{}).Client(ctx, &oauth2.Token{AccessToken: accessToken})
+		resp, err := client.Get("https://discord.com/api/users/@me")
+		if err != nil {
+			return nil, err
+		}
 
-	resp, err := client.Get("https://discord.com/api/users/@me")
-	if err != nil {
-		return nil, err
-	}
+		var user *discordgo.User
+		err = json.NewDecoder(resp.Body).Decode(&user)
+		if err != nil {
+			return nil, err
+		}
 
-	var user *discordgo.User
-	err = json.NewDecoder(resp.Body).Decode(&user)
-	if err != nil {
-		return nil, err
-	}
-
-	c.oauthUserCache.Set(cacheKey, user, 0)
-	return user, nil
+		return user, nil
+	})
 }
 
 func (c *RestClientWithCache) OauthUserGuilds(ctx context.Context, accessToken string) ([]*OauthUserGuild, error) {
-	cacheKey := oauthUserGuildsCacheKey(accessToken)
-	cacheItem := c.oauthUserGuildsCache.Get(cacheKey)
-	if cacheItem != nil {
-		return cacheItem.Value(), nil
-	}
+	return getOrSet(c, oauthUserGuildsCacheKey(accessToken), c.oauthUserGuildsCache, func() ([]*OauthUserGuild, error) {
+		client := (&oauth2.Config{}).Client(ctx, &oauth2.Token{AccessToken: accessToken})
 
-	client := (&oauth2.Config{}).Client(ctx, &oauth2.Token{AccessToken: accessToken})
+		resp, err := client.Get("https://discord.com/api/users/@me/guilds")
+		if err != nil {
+			return nil, err
+		}
 
-	resp, err := client.Get("https://discord.com/api/users/@me/guilds")
-	if err != nil {
-		return nil, err
-	}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		resp.Body.Close()
+		resp.Body = io.NopCloser(bytes.NewReader(body))
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	resp.Body.Close()
-	resp.Body = io.NopCloser(bytes.NewReader(body))
+		var guilds []*OauthUserGuild
+		err = json.NewDecoder(resp.Body).Decode(&guilds)
+		if err != nil {
+			return nil, err
+		}
 
-	var guilds []*OauthUserGuild
-	err = json.NewDecoder(resp.Body).Decode(&guilds)
-	if err != nil {
-		return nil, err
-	}
-
-	c.oauthUserGuildsCache.Set(cacheKey, guilds, 0)
-	return guilds, nil
+		return guilds, nil
+	})
 }
 
 func (c *RestClientWithCache) OauthUserGuild(ctx context.Context, accessToken string, guildID string) (*OauthUserGuild, error) {
