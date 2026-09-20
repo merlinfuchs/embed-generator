41e173c5d2a4aabe5f39b85120334a262914a7b0
Merlin <merlinfuchs2001@gmail.com>
2026-09-21 01:42:36 +0200

    Show the preview for components v2 messages in the editor

--- embedg-app/src/components/EditorMessagePreview.tsx
+++ embedg-app/src/components/EditorMessagePreview.tsx
@@ -7,22 +7,11 @@ export default function EditorMessagePreview() {
   // We debounce the message preview to prevent it from updating too often.
   const msg = useDebouncedCurrentDocument(250)?.message;
 
-  if (msg) {
-    if (msg.flags && (msg.flags & (1 << 15)) !== 0) {
-      return (
-        <div className="p-3 text-mist-300 font-light leading-6">
-          Message preview is currently not available when using Components V2.
-          Just send your message to a test channel to see how it looks.
-        </div>
-      );
-    } else {
-      return (
-        <Suspense>
-          <LazyMessagePreview msg={msg} />
-        </Suspense>
-      );
-    }
-  } else {
-    return null;
-  }
+  if (!msg) return null;
+
+  return (
+    <Suspense>
+      <LazyMessagePreview msg={msg} />
+    </Suspense>
+  );
 }
